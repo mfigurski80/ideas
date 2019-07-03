7bb1c52531eb7957bb3e7cc1f319347f9a9207f8
mfigurski80 <meek.f80@gmail.com>
2019-07-03 10:15:12 -0400

    Added some safety to idea service, added object pruning

--- src/idea/idea.service.ts
+++ src/idea/idea.service.ts
@@ -13,34 +13,52 @@ export class IdeaService {
   ) {}
 
   async showAll() {
-    return await this.ideaRepository.find();
+    return [...await this.ideaRepository.find()];
   }
 
   async create(data:IdeaDTO) {
+    data = this.pick(data, Object.keys(IdeaDTO));
     const idea = await this.ideaRepository.create(data);
     await this.ideaRepository.save(idea);
-    return idea;
+    return {...idea};
   }
 
   async read(id:string) {
     const idea = await this.ideaRepository.findOne({ where: { id } });
     if (!idea) throw new HttpException('Not Found', HttpStatus.NOT_FOUND);
-    return idea;
+    return {...idea};
   }
 
   async update(id:string, data:Partial<IdeaDTO>) {
+    data = this.pick(data, Object.keys(IdeaDTO));
     let idea = await this.ideaRepository.findOne({ where: { id } });
     if (!idea) throw new HttpException('Not Found', HttpStatus.NOT_FOUND);
-    await this.ideaRepository.update({ id }, data);
-    idea = await this.ideaRepository.findOne({ where: { id } });
-    return idea;
+    idea = await this.ideaRepository.save({...idea, ...data});
+    return {...idea};
   }
 
   async destroy(id:string) {
     const idea = await this.ideaRepository.findOne({ where: { id } });
     if (!idea) throw new HttpException('Not Found', HttpStatus.NOT_FOUND);
     await this.ideaRepository.delete({ id });
-    return idea;
+    return {...idea};
+  }
+
+
+
+  /* Utility Methods */
+
+  /**
+   * Picks specific paths from object
+   * @param  obj   Object to pick from
+   * @param  paths Paths picked
+   * @return       Cleaned object
+   */
+  private pick(obj, paths) {
+    Object.keys(obj)
+      .filter(key => !paths.includes(key))
+      .forEach(key => delete obj[key]);
+    return obj;
   }
 
 }
